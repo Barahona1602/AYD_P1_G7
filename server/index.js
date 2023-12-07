f40fe10509bfe698a767a247e4d5cb840250e288
Pablo <pbarahona07@gmail.com>
2023-12-07 13:00:28 -0600

    [fix] back

--- server/index.js
+++ server/index.js
@@ -27,47 +27,11 @@ connection.connect((err) => {
 });
 
 
-//Endpoint de login
-app.post('/login', (req, res) => {
-    const { usuario, contraseña } = req.body;
-
-    const usuarioValido = 'admin';
-    const contraseñaValida = 'admin';
-
-    if (usuario === usuarioValido && contraseña === contraseñaValida) {
-        res.status(200).send('Acceso concedido');
-    } else {
-        res.status(401).send('Usuario o contraseña incorrectos');
-    }
-});
-
-
 // Ruta de prueba
 app.get('/', (req, res) => {
     res.send('¡Hola, mundo!');
 });
 
-app.post('/RegistrarUsuario', (req, res) => {
-  // Obtener datos del formulario de registro
-  const { nombre, apellido, telefono, correo, contraseña, fechaNacimiento } = req.body;
-
-  // Validar la contraseña (mínimo 8 caracteres, incluir una mayúscula y números)
-  const contraseñaValida = /^(?=.*\d)(?=.*[A-Z]).{8,}$/.test(contraseña);
-
-  if (!contraseñaValida) {
-    return res.status(400).json({ mensaje: 'La contraseña no cumple con los requisitos' });
-  }
-
-  // Puedes agregar más validaciones según tus necesidades
-
-
-  // Aquí deberías guardar la información en tu base de datos o realizar cualquier otra acción necesaria
-
-
-  
-  // Respuesta exitosa
-  res.json({ mensaje: 'Usuario registrado con éxito', usuario: { nombre, apellido, telefono, correo, fechaNacimiento } });
-});
 
 // Iniciar el servidor
 app.listen(port, () => {
