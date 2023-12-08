75c41145f8c603bcf1f91ef6cc3c25f97f73f7e8
Ryan1906 <98917762+Ryan1906@users.noreply.github.com>
2023-12-08 13:52:37 -0600

    Users Registered Finished

--- server/index.js
+++ server/index.js
@@ -2,35 +2,66 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const mysql = require('mysql');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+//Conexión a db
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345',
+    database: 'mylibrary',
+    port: 3306
+});
+
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+    } else {
+        console.log('Conexión exitosa a la base de datos');
+    }
+});
+
+
 
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
 app.post('/RegistrarUsuario', (req, res) => {
-  // Obtener datos del formulario de registro
-  const { nombre, apellido, telefono, correo, contraseña, fechaNacimiento } = req.body;
-
-  // Validar la contraseña (mínimo 8 caracteres, incluir una mayúscula y números)
-  const contraseñaValida = /^(?=.*\d)(?=.*[A-Z]).{8,}$/.test(contraseña);
-
-  if (!contraseñaValida) {
-    return res.status(400).json({ mensaje: 'La contraseña no cumple con los requisitos' });
-  }
-
-  // Puedes agregar más validaciones según tus necesidades
-
-
-  // Aquí deberías guardar la información en tu base de datos o realizar cualquier otra acción necesaria
-
-
+    // Obtener datos del formulario de registro
+    const { nombre, apellido, telefono, correo, contraseña, fechaNacimiento } = req.body;
   
-  // Respuesta exitosa
-  res.json({ mensaje: 'Usuario registrado con éxito', usuario: { nombre, apellido, telefono, correo, fechaNacimiento } });
-});
+    // Validar la contraseña (mínimo 8 caracteres, incluir una mayúscula y números)
+    const contraseñaValida = /^(?=.*\d)(?=.*[A-Z]).{8,}$/.test(contraseña);
+  
+    if (!contraseñaValida) {
+      return res.status(400).json({ mensaje: 'La contraseña no cumple con los requisitos' });
+    }
+  
+    // Query SQL para insertar un nuevo usuario
+    const sql = 'INSERT INTO usuarios (nombre, apellido, numero_tel, correo, password, fecha_nac) VALUES (?, ?, ?, ?, ?, ?)';
+  
+    // Parámetros para la consulta
+    const values = [nombre, apellido, telefono, correo, contraseña, fechaNacimiento];
+  
+    // Ejecutar la consulta
+    connection.query(sql, values, (err, results) => {
+      if (err) {
+        console.error('Error al registrar usuario en la base de datos:', err);
+        res.status(500).json({ mensaje: 'Error al registrar usuario' });
+      } else {
+        console.log('Usuario registrado con éxito');
+        res.json({ mensaje: 'Usuario registrado con éxito', usuario: { nombre, apellido, telefono, correo, fechaNacimiento } });
+      }
+    });
+  });
+  
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
