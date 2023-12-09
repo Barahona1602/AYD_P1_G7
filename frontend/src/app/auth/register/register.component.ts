3407edf61d15e2ab5d086bf2e9fc70e27e1f9501
pereznator <3004272120101@ingenieria.usac.edu.gt>
2023-12-08 19:09:10 -0600

    formulario de registro terminado

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -30,4 +30,27 @@ export class RegisterComponent implements OnInit {
   get notValidNombre(): boolean {
     return this.registerForm.get("nombre").touched && this.registerForm.get("nombre").invalid;
   }
+  get notValidApellido(): boolean {
+    return this.registerForm.get("apellido").touched && this.registerForm.get("apellido").invalid;
+  }
+  get notValidTelefono(): boolean {
+    return this.registerForm.get("telefono").touched && this.registerForm.get("telefono").invalid;
+  }
+  get notValidFechaNacimiento(): boolean {
+    return this.registerForm.get("fechaNacimiento").touched && this.registerForm.get("fechaNacimiento").invalid;
+  }
+  get notValidCorreo(): boolean {
+    return this.registerForm.get("correo").touched && this.registerForm.get("correo").invalid;
+  }
+  get notValidPassword(): boolean {
+    return this.registerForm.get("password").touched && this.registerForm.get("password").invalid;
+  }
+  get notValidPasswordRepeat(): boolean {
+    if (this.registerForm.get("passwordRepeat").touched && this.registerForm.get("passwordRepeat").invalid) {
+      return true;
+    }
+    return this.registerForm.get("passwordRepeat").value !== this.registerForm.get("password").value;
+  }
+
+
 }
